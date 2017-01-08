a3fe502dd9066bf6cba05d9037f1bad9ef367be0
foukLor <vitaminotar@yandex.com>
2017-01-08 12:45:58 +0300

    useful printing added. unnessesary lines deleted

--- image_resize.py
+++ image_resize.py
@@ -20,17 +20,22 @@ def get_image_by_path(path_to_img):
 
 def resize_image(arguments):
     image = get_image_by_path(arguments.path_to_img)
-    width = arguments.width
-    height = arguments.height
-    scale = arguments.scale
-    if scale is not None:
-        if width or height is not None:
+    if arguments.scale is not None:
+        if arguments.width or arguments.height is not None:
             return None
-        new_width, new_height = get_new_image_size_by_scale(image, scale)
+        new_width, new_height = get_new_image_size_by_scale(image, arguments.scale)
     else:
-        new_width, new_height = get_new_image_size_by_parametrs(image, width, height)
+        new_width, new_height = get_new_image_size_by_parametrs(image, arguments.width, arguments.height)
+    print_warning_before_resized()
     new_image = image.resize((new_width, new_height))
     return new_image
+
+
+def print_warning_before_resized():
+    print("Your image will be resized")
+
+def print_new_image_name(image_name):
+    print("New name - {0}".format(image_name))
 
 
 def get_new_image_size_by_scale(image, scale):
@@ -46,6 +51,7 @@ def get_new_image_size_by_parametrs(image, width, height):
 def save_image(image, path_to_save):
     new_path_to_save = get_new_image_name(image.width, image.height, path_to_save)
     image.save(new_path_to_save)
+    print_new_image_name(new_path_to_save)
 
 
 def get_new_image_name(width, height, full_path_to_img):
@@ -57,6 +63,9 @@ def get_new_image_name(width, height, full_path_to_img):
 
 if __name__ == '__main__':
     arguments = parse_arguments()
+    if arguments.path_to_img is None:
+        print("You forgot about path to the image")
+        exit()
     if arguments.output is not None:
         path_to_img = os.path.join(arguments.output, os.path.split(arguments.path_to_img)[1])
     else:
